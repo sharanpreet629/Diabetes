caf74501d64a95fdf7dfd7eda3c57061d6719adc
Sharanpreet Kaur <83025267+sharanpreet629@users.noreply.github.com>
2021-07-03 15:46:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+#Importing the libraries
+import pickle
+import numpy
+from flask import Flask, request , render_template
+
+
+#Global Variables
+app = Flask(__name__)
+loadedModel = pickle.load(open('diabetes.pkl', 'rb'))
+
+
+
+#User defined Functions
+@app.route('/', methods=['GET'])
+def Home():
+    return render_template('diabetes.html')
+
+@app.route('/prediction', methods= ['POST'])
+def predict():
+    name = request.form['name']
+    bmi = int(request.form['bmi'])
+    age = int(request.form['age'])
+    glucose = int(request.form['glucose'])
+
+    prediction= loadedModel.predict([[glucose,bmi,age]])
+
+    if prediction==[0]:
+        prediction='Not Diabetic'
+    else:
+        prediction ='Diabetic'
+
+    return render_template('diabetes.html', diagnosis_output= prediction)
+
+
+
+
+
+#Main function
+if __name__ == "__main__":
+    app.run(debug = True)
