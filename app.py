790fd9224fbf3b3855ed34ec40266d334cb908d8
Sharanpreet Kaur <83025267+sharanpreet629@users.noreply.github.com>
2021-07-09 15:55:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,13 +23,16 @@ def predict():
     glucose = int(request.form['glucose'])
 
     prediction= loadedModel.predict([[glucose,bmi,age]])
+    chances = loadedModel.predict_proba([[glucose,bmi,age]])
 
     if prediction==[0]:
         prediction='Not Diabetic'
     else:
         prediction ='Diabetic'
+        
+    showChances = str(round((np.amax(chances[0])*100),2))
 
-    return render_template('diabetes.html', diagnosis_output= prediction)
+    return render_template('diabetes.html', diagnosis_output= prediction, confidence_output = showChances)
 
 
 
